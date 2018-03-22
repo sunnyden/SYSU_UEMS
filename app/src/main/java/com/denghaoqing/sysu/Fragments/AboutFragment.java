8f0650fb9115cd6fc6f97b218c8d0537d1463776
Haoqing Deng <exeded@126.com>
2018-03-22 08:01:39 +0800

    Implement updater open button

--- app/src/main/java/com/denghaoqing/sysu/Fragments/AboutFragment.java
+++ app/src/main/java/com/denghaoqing/sysu/Fragments/AboutFragment.java
@@ -21,14 +21,18 @@
 package com.denghaoqing.sysu.Fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.denghaoqing.sysu.R;
+import com.denghaoqing.sysu.Updater;
+import com.denghaoqing.sysu.Utils.Software;
 
 /**
  * Created by sunny on 18-3-13.
@@ -45,7 +49,19 @@ public class AboutFragment extends Fragment {
         } catch (Exception e) {
             textView.setText("N/A");
         }
-
+        Button updateBtn = view.findViewById(R.id.update_button);
+        if (Software.FLAG_NEW_VERSION) {
+            updateBtn.setVisibility(View.VISIBLE);
+        } else {
+            updateBtn.setVisibility(View.GONE);
+        }
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(getContext(), Updater.class);
+                getContext().startActivity(mIntent);
+            }
+        });
         return view;
     }
 }
